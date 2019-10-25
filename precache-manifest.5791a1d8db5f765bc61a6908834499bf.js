78962a6428ffaa55dd088374207f308cbdc5da3c
hack89 <hackinsta89@gmail.com>
2019-10-25 19:32:18 +0200

    Updates

---
+++ precache-manifest.5791a1d8db5f765bc61a6908834499bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ada8b5c0acf88b90531771d739fd9e5",
+    "url": "/monster_finder_react/index.html"
+  },
+  {
+    "revision": "249f6089e37f5c83bec9",
+    "url": "/monster_finder_react/static/css/main.af9d1724.chunk.css"
+  },
+  {
+    "revision": "d69af6b72f807a28a171",
+    "url": "/monster_finder_react/static/js/2.7f36ca1c.chunk.js"
+  },
+  {
+    "revision": "249f6089e37f5c83bec9",
+    "url": "/monster_finder_react/static/js/main.c6b35916.chunk.js"
+  },
+  {
+    "revision": "a7b49b19ddb82f4e3a33",
+    "url": "/monster_finder_react/static/js/runtime-main.5519f5b2.js"
+  }
+]);
